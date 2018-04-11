cda8a2ddbefdcb4100c089a65b6c324ed643651e
Jeremy Hynes <jeremy.hynes@telecomnancy.net>
2018-04-11 11:29:24 +0200

    Debut

--- main.c
+++ main.c
@@ -18,70 +18,22 @@
 #define BUFSIZE 1500  /* Taille du MTU */
 #define MAXLINE 80
 
-
-int main (int argc, char *argv[])
-
-{
-  int rawSocket, n,lenIPHeader, lenIPHeader2, sport, dport;
-  struct sockaddr_in  fromAddr;
-  socklen_t len;
-  char source[BUFSIZE],rcvbuffer[BUFSIZE];;
-  struct ip *ip, *ip2;
-  struct icmp *icmp;
-  struct udphdr *udp;
-
-  if ((rawSocket = socket(AF_INET,SOCK_RAW,IPPROTO_ICMP)) <0) {
-    perror ("erreur socket");
-    exit (1); }
-  for (;;) {
-    len = sizeof(fromAddr);
-    if ( (n= recvfrom (rawSocket,rcvbuffer, BUFSIZE,0,
-		     (struct sockaddr *)&fromAddr, &len)) < 0 )  {
-      printf ("erreur recvfrom");
-      exit (1);}
-
-    if ( inet_ntop (PF_INET, (const void *)&fromAddr.sin_addr,source,sizeof(source)) <0) {
-      printf ("erreur inet_ntop");
-      exit (1); }
-    printf( "%d octets ICMP de %s: \n", n, source);
-
-    ip = (struct ip*)rcvbuffer;                           // debut entete IP
-    lenIPHeader = ip->ip_hl * 4;       // ip->ip_hl longueur  en mot de 32 bits
-
-    icmp =  (struct icmp *) (rcvbuffer + lenIPHeader) ;             // debut entete ICMP
-    ip2 =  (struct ip*)(rcvbuffer + lenIPHeader +8);               // debut en-tete IP contenu dans ICMP
-    lenIPHeader2 = ip2->ip_hl*4;       // longueur  en-tete IP
-    if (ip2->ip_p == IPPROTO_UDP) {
-      udp = (struct udphdr *) (rcvbuffer + lenIPHeader +8 + lenIPHeader2);                      // debut en-tete UDP
-      sport = ntohs(udp->source);
-      dport = ntohs(udp->dest) ;
-      printf (" port source = %d et port destination = %d \n", sport, dport);
-    }
-    switch (icmp->icmp_type) {
-    case ICMP_UNREACH: {
-      printf ("destination unreachable \n");
-      switch (icmp->icmp_code){
-      case ICMP_UNREACH_PORT:
-	printf (" bad port \n");
-	break;
-      default:
-	printf ("type %d, code = %d\n", icmp->icmp_type,
-	   icmp->icmp_code);
-	break;
-      } }
-      break;
-    case ICMP_ECHO:
-      printf (" echo service  \n");
-      break;
-    case ICMP_ECHOREPLY :
-      printf (" echo reply  \n");
-      break;
-    case ICMP_TIMXCEED :
-      printf (" Time Exceed \n");
-      break;
-    default:
-      printf ("type %d, code = %d\n", icmp->icmp_type,
-	   icmp->icmp_code);
-    }
+void checkPorts(){
+  int sockfd;
+  int on=1;
+  if ((sockfd=socket(AF_INET,SOCK_RAW,IPPROTO_ICMP))<0) {
+    perror("Socket Error: ");
+    exit(1);
   }
+
+  if (setsockopt(sockfd,IPPROTO_IP,IP_HDRINCL,&on,sizeof(on))<0) {
+    perror("SocketOpt Error: ");
+    exit(1);
+  }
+
+}
+
+int main (int argc, char *argv[]){
+  checkPorts();
+  printf("Check Ports: Done.\n");
 }
